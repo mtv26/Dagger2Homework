f5996625373631ee8f93e3c72663f2f4789996b2
Alexey Matveev <m.alex@mail.communigate.ru>
2025-05-14 06:02:07 +0300

    ApplicationComponent

--- app/src/main/java/ru/otus/daggerhomework/App.kt
+++ app/src/main/java/ru/otus/daggerhomework/App.kt
@@ -2,4 +2,13 @@ package ru.otus.daggerhomework
 
 import android.app.Application
 
-class App :Application()
+class App : Application() {
+
+    lateinit var appComponent: ApplicationComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        appComponent = DaggerApplicationComponent.factory().create(this)
+    }
+
+}
--- app/src/main/java/ru/otus/daggerhomework/ApplicationComponent.kt
+++ app/src/main/java/ru/otus/daggerhomework/ApplicationComponent.kt
@@ -1,4 +1,19 @@
 package ru.otus.daggerhomework
 
+import android.content.Context
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component
 interface ApplicationComponent {
+
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance @ApplicationContext context: Context): ApplicationComponent
+    }
+
+    @get:ApplicationContext
+    val applicationContext: Context
 }
